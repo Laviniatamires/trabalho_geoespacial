fa7513708fe375dc44c08544565b573c597a691f
Laviniatamires <laviniatamires2011@gmail.com>
2017-10-27 15:11:50 -0200

    enviado

---
+++ lavinia.js
@@ -0,0 +1,154 @@
+db.places.insert( {
+    name: "Mércia",
+   location: { type: "Point", coordinates: [-43.0685685,-15.5343928] }
+} );
+
+db.places.insert( {
+    name: "Luana",
+   location: { type: "Point", coordinates: [ -43.3160809,-15.8052074 ] }
+} );
+
+db.places.insert( {
+    name: "Joana",
+   location: { type: "Point", coordinates: [ -43.310403 ,-15.7943479  ] }
+} );
+
+db.places.insert( {
+    name: "Alberto",
+   location: { type: "Point", coordinates: [-43.3179225 ,-15.8057438 ] }
+} );
+
+db.places.insert( {
+    name: "Layla",
+   location: { type: "Point", coordinates: [ -43.3179687,-15.8198619 ] }
+} );
+
+db.places.insert( {
+    name: "Thalia",
+   location: { type: "Point", coordinates: [ -43.341919,-15.792172 ] }
+} );
+
+db.places.insert( {
+    name: "Emilly R",
+   location: { type: "Point", coordinates: [ -43.312611, -15,807591] }
+} );
+
+db.places.insert( {
+    name: "Savia",
+   location: { type: "Point", coordinates: [-43.3223997 ,-15.7814025 ] }
+} );
+
+db.places.insert( {
+    name: "Diyse",
+   location: { type: "Point", coordinates: [-43,323578 ,-15.781380 ] }
+} );
+
+db.places.insert( {
+    name: "Vitoria I",
+   location: { type: "Point", coordinates: [-43.314893 ,-15.817489 ] }
+} );
+
+db.places.insert( {
+    name: "Joao V",
+   location: { type: "Point", coordinates: [ -43.306551,-15.8147075 ] }
+} );
+
+db.places.insert( {
+    name: "Michelle",
+   location: { type: "Point", coordinates: [ -43.3123914,-15.8263155 ] }
+} );
+
+db.places.insert( {
+    name: "Maria",
+   location: { type: "Point", coordinates: [ -43.0288791,-15.7377377 ] }
+} );
+
+db.places.insert( {
+    name: "Adrian",
+   location: { type: "Point", coordinates: [-43.304557 ,-15.811190 ] }
+} );
+
+db.places.insert( {
+    name: "Lara",
+   location: { type: "Point", coordinates: [ -43,3153666,-15.8197566 ] }
+} );
+
+db.places.insert( {
+    name: "Camila",
+   location: { type: "Point", coordinates: [ -43.2960149,-15.8251198 ] }
+} );
+
+db.places.insert( {
+    name: "Livia",
+   location: { type: "Point", coordinates: [-43.314748 ,-15.7953561 ] }
+} );
+
+db.places.insert( {
+    name: "Micaelle",
+   location: { type: "Point", coordinates: [ -43.3148092,-15.8221044 ] }
+} );
+
+db.places.insert( {
+    name: "Flora",
+   location: { type: "Point", coordinates: [ -43.3051085,-15.8018395 ] }
+} );
+
+db.places.insert( {
+    name: "Jade",
+   location: { type: "Point", coordinates: [ -43.309728, -15.819087] }
+} );
+
+db.places.insert( {
+    name: "Sidney",
+   location: { type: "Point", coordinates: [-43.3121088 ,-15.80360014 ] }
+} );
+
+db.places.insert( {
+    name: "Lavinia",
+   location: { type: "Point", coordinates: [-43.3168428 ,-15.8045297 ] }
+} );
+
+db.places.insert( {
+    name: "Victoria F",
+   location: { type: "Point", coordinates: [-43.300751 ,-15.818761 ] }
+} );
+
+db.places.insert( {
+    name: "Emille B",
+   location: { type: "Point", coordinates: [ -43.3125666,-15.8039344 ] }
+} );
+
+db.places.insert( {
+    name: "Hellen",
+   location: { type: "Point", coordinates: [ -43.3165794, -15.7990549] }
+} );
+
+db.places.insert( {
+    name: "Joyce",
+   location: { type: "Point", coordinates: [ -43.3087511,-15.8290764 ] }
+} );
+
+db.places.insert( {
+    name: "Amanda",
+   location: { type: "Point", coordinates: [ -43.314899,-15.802350 ] }
+} );
+
+db.places.insert( {
+    name: "Luan",
+   location: { type: "Point", coordinates: [ -43.3138138,-15.8007558 ] }
+} );
+
+db.places.insert( {
+    name: "Joao G",
+   location: { type: "Point", coordinates: [-43.3049733 , -15.8489078] }
+} );
+
+db.places.insert( {
+    name: "Daniel",
+   location: { type: "Point", coordinates: [-43.3084923 ,-15.8025955 ] }
+} );
+
+db.places.insert( {
+    name: "Tom",
+   location: { type: "Point", coordinates: [ -43.3090244,-15.802596 ] }
+} );
